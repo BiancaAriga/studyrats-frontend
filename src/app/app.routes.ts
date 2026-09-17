840fa47f24f8f40070d450681d3c0d8b63f1b617
biancaariga <biancaariga45@gmail.com>
2026-09-17 18:34:58 -0300

    feat: dashboard guard

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,9 +1,16 @@
 import { Routes } from '@angular/router';
 import { Login } from './pages/login/login';
+import { Dashboard } from './pages/dashboard/dashboard';
+import { authGuard } from './core/auth/auth-guard';
 
 export const routes: Routes = [
   {
     path: 'login',
     component: Login,
+  },
+  {
+    path: 'dashboard',
+    component: Dashboard,
+    canActivate: [authGuard],
   },
 ];
