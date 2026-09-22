f1c6fe7b1c795ef4fb835064160d531c18b6ef7a
biancaariga <biancaariga45@gmail.com>
2026-09-22 17:58:56 -0300

    feat: template de login

--- src/app/pages/login/login.ts
+++ src/app/pages/login/login.ts
@@ -1,15 +1,17 @@
 import { Component, inject } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { Auth } from '../../services/auth';
+import { Router, RouterLink } from '@angular/router';
 
 @Component({
-  imports: [FormsModule],
+  imports: [FormsModule, RouterLink],
   selector: 'app-login',
   styleUrl: './login.css',
   templateUrl: './login.html',
 })
 export class Login {
   private auth = inject(Auth);
+  private router = inject(Router);
 
   email = '';
   password = '';
@@ -18,7 +20,7 @@ export class Login {
     this.auth.login(this.email, this.password).subscribe({
       next: (response) => {
         this.auth.saveToken(response.access_token);
-        console.log('Login realizado com sucesso');
+        this.router.navigate(['/dashboard']);
       },
       error: (error) => {
         console.error(error);
