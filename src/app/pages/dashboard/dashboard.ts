23f2f1607b64b3fde3d3cea0498fa725b43e5aa4
biancaariga <biancaariga45@gmail.com>
2026-09-26 11:59:28 -0300

    feat: ajustes finais do dashboard

--- src/app/pages/dashboard/dashboard.ts
+++ src/app/pages/dashboard/dashboard.ts
@@ -1,6 +1,8 @@
 import { Component, inject, signal } from '@angular/core';
 import { Auth } from '../../services/auth';
 import { Quote } from '../../services/quote';
+import { StudySession } from '../../services/study-session';
+import { Ranking } from '../../services/ranking';
 
 @Component({
   imports: [],
@@ -11,17 +13,43 @@ import { Quote } from '../../services/quote';
 export class Dashboard {
   private auth = inject(Auth);
   private quote = inject(Quote);
+  private studySession = inject(StudySession);
+  private rankingService = inject(Ranking);
 
   userName = signal('');
   quoteText = signal('');
   quoteAuthor = signal('');
+  rankingPosition = signal<number | null>(null);
+  ranking = signal<
+    {
+      user_id: number;
+      user_name: string;
+      total_duration: number;
+    }[]
+  >([]);
+  totalStudyTime = signal(0);
+  sessionCount = signal(0);
 
   ngOnInit() {
     this.auth.getMe().subscribe({
       next: (user) => {
         this.userName.set(user.name);
+
+        this.rankingService.getRanking().subscribe({
+          next: (ranking) => {
+            this.ranking.set(ranking);
+            const position = ranking.findIndex(
+              (rankingUser) => rankingUser.user_id === user.id,
+            );
+
+            if (position !== -1) {
+              this.rankingPosition.set(position + 1);
+            }
+          },
+        });
       },
     });
+
     this.quote.getRandomQuote().subscribe({
       next: (response) => {
         this.quoteText.set(response.quote);
@@ -29,6 +57,28 @@ export class Dashboard {
       },
       error: (error) => {
         console.error('Erro ao buscar quote:', error);
+      },
+    });
+
+    this.studySession.getSessions().subscribe({
+      next: (response) => {
+        const sessions = response as {
+          id: number;
+          subject: string;
+          duration: number;
+        }[];
+
+        this.sessionCount.set(sessions.length);
+
+        const total = sessions.reduce(
+          (sum, session) => sum + session.duration,
+          0,
+        );
+
+        this.totalStudyTime.set(total);
+      },
+      error: (error) => {
+        console.error('Erro ao buscar sessões:', error);
       },
     });
   }
